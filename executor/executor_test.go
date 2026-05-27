cf163ed54b33ab7cf25e6cad3282bc4861ddb873
omaritooo <themdic227@gmail.com>
2026-05-27 17:42:45 +0300

    chore: add testify dependency

---
+++ executor/executor_test.go
@@ -0,0 +1,12 @@
+package executor_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// placeholder test to anchor the testify dependency
+func TestPlaceholder(t *testing.T) {
+	assert.True(t, true)
+}
